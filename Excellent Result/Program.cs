d484b241cca89040303f114c35bd09340eab96ce
its julii <nanji828@gmail.com>
2022-11-05 20:08:12 +0200

    Add project files.

---
+++ Excellent Result/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Excellent_Result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rating = double.Parse(Console.ReadLine());
+            if (rating >= 5.50)
+            {
+                Console.WriteLine("Exelent");
+            }
+
+
+
+        }
+    }
+}
